16b2098fc372c028d5e35e1e02b771b7f0650a46
Barrett Clark <code@cookingco.de>
2016-12-01 23:15:14 -0600

    Day 1, Part 2

--- day01/block_counter.rb
+++ day01/block_counter.rb
@@ -1,5 +1,5 @@
 class BlockCounter
-  attr_reader :blocks_moved
+  attr_reader :coordinates_visited
 
   HEADINGS = {
     :north => "+",
@@ -9,8 +9,14 @@ class BlockCounter
   }
 
   def initialize
-    @heading      = :north
-    @blocks_moved = 0
+    @heading = :north
+    @coordinate = [0, 0]
+    @coordinates_visited = [@coordinate]
+  end
+
+  def blocks_moved
+    x, y = @coordinate
+    (x + y).abs
   end
 
   def parse_directions(input)
@@ -19,7 +25,7 @@ class BlockCounter
 
   def walk!
     @directions.each { |step| move(step) }
-    puts "We walked #{@blocks_moved.abs} blocks"
+    puts "We walked #{blocks_moved} blocks"
   end
 
   private
@@ -27,13 +33,40 @@ class BlockCounter
   def move(step)
     direction, count = parse_step(step)
     set_heading!(direction)
-    @blocks_moved = @blocks_moved.send(HEADINGS[@heading], count)
+    generate_coordinates(count)
   end
 
   def set_heading!(direction)
     bearing     = HEADINGS.keys.index(@heading)
     new_bearing = direction == "R" ? bearing + 1 : bearing - 1
     @heading    = HEADINGS.keys[new_bearing%4]
+  end
+
+  def generate_coordinates(count)
+    # coordinates
+    x, y = @coordinate
+    case @heading
+    when :north
+      # Y + step
+      1.upto(count) { add_coordinate([x, y+=1]) }
+    when :south
+      # Y - step
+      1.upto(count) { add_coordinate([x, y-=1]) }
+    when :east
+      # X + step
+      1.upto(count) { add_coordinate([x+=1, y]) }
+    when :west
+      # X - step
+      1.upto(count) { add_coordinate([x-=1, y]) }
+    end
+  end
+
+  def add_coordinate(coordinate)
+    @coordinate = coordinate
+    if @coordinates_visited.include?(@coordinate)
+      puts "#{coordinate} #{blocks_moved} blocks away, due #{@heading}"
+    end
+    @coordinates_visited << coordinate
   end
 
   def parse_step(step)
@@ -44,9 +77,9 @@ end
 
 if __FILE__==$0
   block_counter = BlockCounter.new
-  # block_counter.parse_directions("R2, L3")
-  # block_counter.parse_directions("R2, R2, R2")
-  # block_counter.parse_directions("R5, L5, R5, R3")
+  # input = "R2, L3"
+  # input = "R2, R2, R2"
+  # input = "R5, L5, R5, R3"
   input = File.read('input.txt')
   block_counter.parse_directions(input)
   block_counter.walk!
