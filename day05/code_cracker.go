ca8abcfa0f778a0168ee0d2aebb7030d89503182
Barrett Clark <code@cookingco.de>
2016-12-07 00:27:51 -0600

    Day 5, puzzle 2

--- day05/code_cracker.go
+++ day05/code_cracker.go
@@ -9,16 +9,35 @@ import (
 	"strings"
 )
 
-func generate_character(prefix string, min int, max int) (int, string) {
+func generate_character(prefix string, min int, max int) (int, int, string) {
+	var h [16]byte
+	var hash string
+	var match bool
+	var position int
+	var character string
 	for i := min; i <= max; i++ {
-		h := md5.Sum([]byte(prefix + strconv.Itoa(i)))
-		hash := (hex.EncodeToString(h[:]))
-		match, _ := regexp.MatchString("^0{5}", hash)
+		h = md5.Sum([]byte(prefix + strconv.Itoa(i)))
+		hash = (hex.EncodeToString(h[:]))
+		match, _ = regexp.MatchString("^0{5}[0-7]", hash)
 		if match {
-			return i, hash[5:6]
+			position, _ = strconv.Atoi(hash[5:6])
+			character = hash[6:7]
+			fmt.Println(i, hash, position, character)
+			return i, position, character
 		}
 	}
-	return 0, ""
+	return 0, -1, ""
+}
+
+// "trivial" yet super specific array contains function
+// http://stackoverflow.com/questions/10485743/contains-method-for-a-slice
+func contains(s [8]string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
@@ -26,17 +45,17 @@ func main() {
 	prefix := "wtnhxymk"
 	min := 1
 	max := int(1 << 50)
-	idx := 0
 	var characters [8]string
 
 	for {
-		i, character := generate_character(prefix, min, max)
-		characters[idx] = character
-		fmt.Println(character, characters)
-		if characters[7] != "" {
-			break
+		i, idx, character := generate_character(prefix, min, max)
+		if characters[idx] == "" {
+			characters[idx] = character
+			fmt.Println(character, characters)
+			if contains(characters, "") == false {
+				break
+			}
 		}
-		idx++
 		min = i + 1
 	}
 	fmt.Println(strings.Join(characters[:], ""))
