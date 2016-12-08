82381eb58989468f0dbf7378a053b55e4cd9d227
Barrett Clark <code@cookingco.de>
2016-12-07 20:28:11 -0600

    Dat 6, puzzle 1

---
+++ day06/repetition_decoder.rb
@@ -0,0 +1,15 @@
+# FILENAME = "input_example.txt"
+FILENAME = "input.txt"
+
+arr = nil
+File.foreach(FILENAME) do |line|
+  characters = line.chomp.chars
+  arr = Array.new(characters.count) { Hash.new(0) } if arr.nil?
+  characters.each_with_index do |char, idx|
+    arr[idx][char] += 1
+  end
+end
+letters = arr.map do |column|
+  column.sort_by { |k,v| -v }.first.first
+end
+p letters.join
