bbd7d26484753a46f6a237d83181a1b1ea696bf8
Barrett Clark <code@cookingco.de>
2017-12-11 20:34:08 -0600

    Day 2, solution 2

--- 2017/day02/spreadsheet_checksum.go
+++ 2017/day02/spreadsheet_checksum.go
@@ -4,7 +4,9 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -15,32 +17,48 @@ func check(e error) {
 	}
 }
 
+func numbersFromLine(line string) (numbers []int) {
+	numberStrings := strings.Split(line, "\t")
+	for _, s := range numberStrings {
+		currentNumber, _ := strconv.Atoi(s)
+		numbers = append(numbers, currentNumber)
+	}
+	return
+}
+
+func sortedMod(n1 int, n2 int) float64 {
+	floats := []float64{float64(n1), float64(n2)}
+	sort.Float64s(floats)
+	return math.Mod(floats[1], floats[0])
+}
+
+func sortedDiv(n1 int, n2 int) int {
+	ints := []int{n1, n2}
+	sort.Ints(ints)
+	return ints[1] / ints[0]
+}
+
 func main() {
-	// file, err := os.Open("example1.txt")
-	file, err := os.Open("input1.txt")
+	file, err := os.Open("input2.txt")
 	check(err)
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	checksum := 0
-	MinInt := int(^uint(0) >> 1)
-	MaxInt := -MinInt
 	for scanner.Scan() {
-		line := scanner.Text()
-		numbers := strings.Split(line, "\t")
-		min := MinInt
-		max := MaxInt
-		for _, n := range numbers {
-			currentNumber, _ := strconv.Atoi(n)
-			if currentNumber < min {
-				min = currentNumber
-			}
-			if currentNumber > max {
-				max = currentNumber
+		numbers := numbersFromLine(scanner.Text())
+		for _, currentNumber := range numbers {
+			for _, compareNumber := range numbers {
+				if currentNumber <= compareNumber {
+					continue
+				} else if sortedMod(currentNumber, compareNumber) == 0 {
+					chk := sortedDiv(currentNumber, compareNumber)
+					checksum += chk
+					// fmt.Println(currentNumber, compareNumber, chk)
+				}
 			}
 		}
-		checksum += max - min
-		// fmt.Printf("Line: %s, Min: %d, Max: %d, Checksum: %d\n", line, min, max, checksum)
+		// fmt.Printf("Line: %v, Checksum: %d\n", numbers, checksum)
 	}
 	fmt.Println(checksum)
 	if err := scanner.Err(); err != nil {
